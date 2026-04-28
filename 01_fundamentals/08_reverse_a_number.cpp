8ea76451f2b750d5fd45ee0bb6d7d909ae4aa881
Devansh Meena <devanshmeena422@gmail.com>
2026-04-28 19:35:59 +0530

    add reverse a number

---
+++ 01_fundamentals/08_reverse_a_number.cpp
@@ -0,0 +1,16 @@
+// Question: Reverse a number.
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n, rev = 0;
+    cin >> n;
+
+    while(n > 0) {
+        int digit = n % 10;
+        rev = rev * 10 + digit;
+        n /= 10;
+    }
+
+    cout << rev;
+}
