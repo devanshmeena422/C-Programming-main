438efa29a74047a83234f503bad0c23a73880393
Devansh Meena <devanshmeena422@gmail.com>
2026-04-28 19:33:10 +0530

    add factorial of n

---
+++ 01_fundamentals/06_calculate_factorial.cpp
@@ -0,0 +1,16 @@
+// Question: Find factorial of a number.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int fact = 1;
+
+    for(int i = 1; i <= n; i++)
+        fact *= i;
+
+    cout << fact;
+}
