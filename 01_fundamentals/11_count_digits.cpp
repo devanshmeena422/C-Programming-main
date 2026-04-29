1e55592d9326fe4d95b7b9d7722a5a948aee733f
Devansh Meena <devanshmeena422@gmail.com>
2026-04-29 19:28:47 +0530

    add count all digit

---
+++ 01_fundamentals/11_count_digits.cpp
@@ -0,0 +1,15 @@
+// Question: Count number of digits in a number.
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n, count = 0;
+    cin >> n;
+
+    while(n > 0) {
+        count++;
+        n /= 10;
+    }
+
+    cout << count;
+}
