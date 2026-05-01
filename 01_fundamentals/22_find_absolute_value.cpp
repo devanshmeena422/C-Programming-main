9f0160bd6b998d52472b304a297d4e39bb131d9c
Devansh Meena <devanshmeena422@gmail.com>
2026-05-01 22:56:09 +0530

    add absolute value

---
+++ 01_fundamentals/22_find_absolute_value.cpp
@@ -0,0 +1,11 @@
+// Question: Find absolute value of a number.
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    if(n < 0)
+        n = -n;
+    cout << n;
+}
