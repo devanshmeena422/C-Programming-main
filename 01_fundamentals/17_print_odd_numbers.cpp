4b619b32e6076069824dba6596ebb901135b0c0b
Devansh Meena <devanshmeena422@gmail.com>
2026-05-01 22:49:55 +0530

    add odd numbers

---
+++ 01_fundamentals/17_print_odd_numbers.cpp
@@ -0,0 +1,10 @@
+// Question: Print odd numbers from 1 to 20.
+
+#include <iostream>
+using namespace std;
+int main() {
+    for(int i = 1; i <= 20; i++) {
+        if(i % 2 != 0)
+            cout << i << " ";
+    }
+}
