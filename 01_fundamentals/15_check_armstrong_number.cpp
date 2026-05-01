bb71b5309505260260ef72b39778f8a0c7fc8d23
Devansh Meena <devanshmeena422@gmail.com>
2026-05-01 22:47:57 +0530

    add Armstrong number

---
+++ 01_fundamentals/15_check_armstrong_number.cpp
@@ -0,0 +1,21 @@
+// Question: Check Armstrong number.
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n, sum = 0, temp;
+    cin >> n;
+
+    temp = n;
+
+    while(n > 0) {
+        int digit = n % 10;
+        sum += digit * digit * digit;
+        n /= 10;
+    }
+
+    if(sum == temp)
+        cout << "Armstrong";
+    else
+        cout << "Not Armstrong";
+}
