6aee66c3930a284f67e82d29a0afc30e4ac24c68
Devansh Meena <devanshmeena422@gmail.com>
2026-05-01 22:57:22 +0530

    add number to n

---
+++ 01_fundamentals/24_print_numbers_1_to_n.cpp
@@ -0,0 +1,10 @@
+// Question: Print numbers from 1 to n.
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++)
+        cout << i << " ";
+}
