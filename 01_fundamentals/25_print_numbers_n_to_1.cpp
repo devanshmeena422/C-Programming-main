13ee4fea7e91eb7fa275ea0b7f58549783b2d019
Devansh Meena <devanshmeena422@gmail.com>
2026-05-03 22:44:35 +0530

    add print of numbers

---
+++ 01_fundamentals/25_print_numbers_n_to_1.cpp
@@ -0,0 +1,10 @@
+// Question: Print numbers from n to 1.
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for(int i = n; i >= 1; i--)
+        cout << i << " ";
+}
