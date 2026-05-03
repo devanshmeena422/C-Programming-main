214f9c563931aeea9f25fc989c0871452ad66b99
Devansh Meena <devanshmeena422@gmail.com>
2026-05-03 22:47:08 +0530

    add total even numbers

---
+++ 01_fundamentals/28_count_even_numbers.cpp
@@ -0,0 +1,13 @@
+// Question: Count even numbers from 1 to n.
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n, count = 0;
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        if(i % 2 == 0)
+            count++;
+    }
+    cout << count;
+}
