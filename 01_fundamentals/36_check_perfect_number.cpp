ca9efccecdfc8b527824d78505dbbdb9d43c38b5
Devansh Meena <devanshmeena422@gmail.com>
2026-05-04 23:30:54 +0530

    check the number is perfect or not

---
+++ 01_fundamentals/36_check_perfect_number.cpp
@@ -0,0 +1,16 @@
+// 36_check_perfect_number.cpp
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n, sum = 0;
+    cin >> n;
+    for(int i = 1; i < n; i++) {
+        if(n % i == 0)
+            sum += i;
+    }
+    if(sum == n)
+        cout << "Perfect";
+    else
+        cout << "Not Perfect";
+}
