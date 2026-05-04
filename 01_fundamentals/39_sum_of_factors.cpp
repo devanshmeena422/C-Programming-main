9642958186b29c24f216acfad72621513746fc2c
Devansh Meena <devanshmeena422@gmail.com>
2026-05-04 23:32:47 +0530

    add sum of all factors

---
+++ 01_fundamentals/39_sum_of_factors.cpp
@@ -0,0 +1,13 @@
+// Question: Find sum of factors of a number.
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n, sum = 0;
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        if(n % i == 0)
+            sum += i;
+    }
+    cout << sum;
+}
