6c95b0eac8f9a27c955860c621453186f17fab98
Devansh Meena <devanshmeena422@gmail.com>
2026-05-04 23:28:30 +0530

    add max in array

---
+++ 02_Arrays/43_find_max_in_array.cpp
@@ -0,0 +1,12 @@
+// Question: Find maximum element in array
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5] = {10,5,20,8,15};
+    int max = arr[0];
+    for(int i = 1; i < 5; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+    }
+    cout << max;
+}
