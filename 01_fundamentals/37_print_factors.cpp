16d658f77ccf03cf6638e5008368882d551bb663
Devansh Meena <devanshmeena422@gmail.com>
2026-05-04 23:31:31 +0530

    add factors of numbers

---
+++ 01_fundamentals/37_print_factors.cpp
@@ -0,0 +1,12 @@
+// Question: Print all factors of a number.
+
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        if(n % i == 0)
+            cout << i << " ";
+    }
+}
