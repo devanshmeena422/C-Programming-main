20daa2b542e7b0946328f24d6d6dfb6fa7ee58c5
Devansh Meena <devanshmeena422@gmail.com>
2026-05-04 23:27:54 +0530

    Commit message: add sum of array

---
+++ 02_Arrays/42_sum_of_array.cpp
@@ -0,0 +1,10 @@
+// Question: Find sum of elements in an array
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5] = {1,2,3,4,5};
+    int sum = 0;
+    for(int i = 0; i < 5; i++)
+        sum += arr[i];
+    cout << sum;
+}
