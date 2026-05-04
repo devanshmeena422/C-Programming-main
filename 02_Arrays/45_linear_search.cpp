fea91ec4b064ce9a1753a3b2e7c6a3b41fdf9371
Devansh Meena <devanshmeena422@gmail.com>
2026-05-04 23:29:26 +0530

    add linear search

---
+++ 02_Arrays/45_linear_search.cpp
@@ -0,0 +1,14 @@
+// Question: Search an element in array
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5] = {10,20,30,40,50};
+    int key = 30;
+    for(int i = 0; i < 5; i++) {
+        if(arr[i] == key) {
+            cout << "Found";
+            return 0;
+        }
+    }
+    cout << "Not Found";
+}
