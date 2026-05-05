72cd8a3de9d453119ad7c1cf690d9b3553b099ca
Devansh Meena <devanshmeena422@gmail.com>
2026-05-05 22:26:46 +0530

    add count odd in array

---
+++ 02_Arrays/48_count_odd_in_array.cpp
@@ -0,0 +1,12 @@
+// Question: Count odd numbers in array
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5] = {1,2,3,4,5};
+    int count = 0;
+    for(int i = 0; i < 5; i++) {
+        if(arr[i] % 2 != 0)
+            count++;
+    }
+    cout << count;
+}
