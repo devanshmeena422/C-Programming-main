faced93b8eab0a6d5ea7164a1221dede677ad2b1
Devansh Meena <devanshmeena422@gmail.com>
2026-05-05 22:29:36 +0530

    add sum even elements

---
+++ 02_Arrays/52_sum_of_even_elements.cpp
@@ -0,0 +1,12 @@
+// Question: Find sum of even elements in array
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5] = {1,2,3,4,6};
+    int sum = 0;
+    for(int i = 0; i < 5; i++) {
+        if(arr[i] % 2 == 0)
+            sum += arr[i];
+    }
+    cout << sum;
+}
