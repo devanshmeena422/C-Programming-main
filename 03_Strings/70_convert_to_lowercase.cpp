ee07969624709a3105d32fbf7ec52531bc89f3dc
Devansh Meena <devanshmeena422@gmail.com>
2026-05-06 22:50:24 +0530

    add lowercase conversion

---
+++ 03_Strings/70_convert_to_lowercase.cpp
@@ -0,0 +1,10 @@
+// Question: Convert string to lowercase
+#include <iostream>
+using namespace std;
+int main() {
+    string str;
+    cin >> str;
+    for(int i = 0; i < str.length(); i++)
+        str[i] = tolower(str[i]);
+    cout << str;
+}
