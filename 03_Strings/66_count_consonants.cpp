f34606623fd726afec88db141c87995fb0719214
Devansh Meena <devanshmeena422@gmail.com>
2026-05-06 22:48:14 +0530

    add count consonants

---
+++ 03_Strings/66_count_consonants.cpp
@@ -0,0 +1,16 @@
+// Question: Count consonants in a string
+#include <iostream>
+using namespace std;
+int main() {
+    string str;
+    cin >> str;
+    int count = 0;
+    for(int i = 0; i < str.length(); i++) {
+        char ch = tolower(str[i]);
+        if(ch >= 'a' && ch <= 'z') {
+            if(!(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'))
+                count++;
+        }
+    }
+    cout << count;
+}
