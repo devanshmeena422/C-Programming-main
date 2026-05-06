5b0250580eb2c867702644eb2cfb8505453ea2f5
Devansh Meena <devanshmeena422@gmail.com>
2026-05-06 22:48:58 +0530

    add count digits string

---
+++ 03_Strings/67_count_digits_in_string.cpp
@@ -0,0 +1,13 @@
+// Question: Count digits in a string
+#include <iostream>
+using namespace std;
+int main() {
+    string str;
+    cin >> str;
+    int count = 0;
+    for(int i = 0; i < str.length(); i++) {
+        if(str[i] >= '0' && str[i] <= '9')
+            count++;
+    }
+    cout << count;
+}
