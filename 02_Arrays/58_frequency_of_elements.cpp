ca5028b89dd4a31b21247e36a3738846506579d5
Devansh Meena <devanshmeena422@gmail.com>
2026-05-06 22:41:49 +0530

    add frequency of elements

---
+++ 02_Arrays/58_frequency_of_elements.cpp
@@ -0,0 +1,14 @@
+// Question: Count frequency of elements in array
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5] = {1,2,2,3,1};
+    for(int i = 0; i < 5; i++) {
+        int count = 0;
+        for(int j = 0; j < 5; j++) {
+            if(arr[i] == arr[j])
+                count++;
+        }
+        cout << arr[i] << " -> " << count << endl;
+    }
+}
