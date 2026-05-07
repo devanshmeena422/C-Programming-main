7721e2f505689798760e0be7e9c18074feaa6e5c
Devansh Meena <devanshmeena422@gmail.com>
2026-05-07 23:42:22 +0530

    add remove spaces

---
+++ 03_Strings/75_remove_spaces.cpp
@@ -0,0 +1,11 @@
+// Question: Remove spaces from string
+#include <iostream>
+using namespace std;
+int main() {
+    string str;
+    getline(cin, str);
+    for(int i = 0; i < str.length(); i++) {
+        if(str[i] != ' ')
+            cout << str[i];
+    }
+}
