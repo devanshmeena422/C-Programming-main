6c4f91defe3d646303f48e7ed3036275155c42f7
Devansh Meena <devanshmeena422@gmail.com>
2026-05-08 22:23:57 +0530

    add remove duplicate characters

---
+++ 03_Strings/88_remove_duplicate_characters.cpp
@@ -0,0 +1,16 @@
+// Question: Remove duplicate characters from string
+#include <iostream>
+using namespace std;
+int main() {
+    string str;
+    cin >> str;
+    for(int i = 0; i < str.length(); i++) {
+        bool duplicate = false;
+        for(int j = 0; j < i; j++) {
+            if(str[i] == str[j])
+                duplicate = true;
+        }
+        if(!duplicate)
+            cout << str[i];
+    }
+}
