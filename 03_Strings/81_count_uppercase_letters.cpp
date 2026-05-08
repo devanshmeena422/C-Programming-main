e5c225e12d442591432214444fc907ecb5b46172
Devansh Meena <devanshmeena422@gmail.com>
2026-05-08 22:19:51 +0530

    add uppercase count

---
+++ 03_Strings/81_count_uppercase_letters.cpp
@@ -0,0 +1,13 @@
+// Question: Count uppercase letters in a string
+#include <iostream>
+using namespace std;
+int main() {
+    string str;
+    getline(cin, str);
+    int count = 0;
+    for(int i = 0; i < str.length(); i++) {
+        if(str[i] >= 'A' && str[i] <= 'Z')
+            count++;
+    }
+    cout << count;
+}
