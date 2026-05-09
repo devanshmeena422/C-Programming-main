6a250daf519ed448e410083733670bbeeeee2366
Devansh Meena <devanshmeena422@gmail.com>
2026-05-09 20:32:43 +0530

    add remove digits

---
+++ 03_Strings/93_print_string_without_digits.cpp
@@ -0,0 +1,11 @@
+// Question: Remove digits from string
+#include <iostream>
+using namespace std;
+int main() {
+    string str;
+    cin >> str;
+    for(int i = 0; i < str.length(); i++) {
+        if(!(str[i] >= '0' && str[i] <= '9'))
+            cout << str[i];
+    }
+}
