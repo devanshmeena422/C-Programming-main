0e1cc5780811b6c221f1a3dcf7440c538065fc8b
Devansh Meena <devanshmeena422@gmail.com>
2026-05-12 23:11:41 +0530

    add zero one pattern

---
+++ 04_Patterns/118_print_zero_one_pattern.cpp
@@ -0,0 +1,13 @@
+// Question: Print 0-1 triangle pattern
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= i; j++) {
+            cout << (i + j) % 2 << " ";
+        }
+        cout << endl;
+    }
+}
