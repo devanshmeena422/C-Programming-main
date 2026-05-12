aecc43391586ad0ef817c8dd2dc1f3095a53f71c
Devansh Meena <devanshmeena422@gmail.com>
2026-05-12 23:11:02 +0530

    add character pyramid

---
+++ 04_Patterns/117_print_character_pyramid.cpp
@@ -0,0 +1,15 @@
+// Question: Print character pyramid pattern
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        char ch = 'A';
+        for(int j = 1; j <= i; j++) {
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+}
