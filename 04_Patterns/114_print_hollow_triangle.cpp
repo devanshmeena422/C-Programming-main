975f65312d98786576de6ad72f12ec07d38ff0b4
Devansh Meena <devanshmeena422@gmail.com>
2026-05-12 23:04:27 +0530

    add hollow triangle

---
+++ 04_Patterns/114_print_hollow_triangle.cpp
@@ -0,0 +1,16 @@
+// Question: Print hollow triangle pattern
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= i; j++) {
+            if(j == 1 || j == i || i == n)
+                cout << "* ";
+            else
+                cout << "  ";
+        }
+        cout << endl;
+    }
+}
