b5ef4709668896c584dae4190ee2537637e99838
Devansh Meena <devanshmeena422@gmail.com>
2026-05-13 23:30:01 +0530

    add swap function

---
+++ 05_Functions/131_swap_numbers_using_function.cpp
@@ -0,0 +1,14 @@
+// Question: Swap two numbers using function
+#include <iostream>
+using namespace std;
+void swapNumbers(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+int main() {
+    int a, b;
+    cin >> a >> b;
+    swapNumbers(a, b);
+    cout << a << " " << b;
+}
