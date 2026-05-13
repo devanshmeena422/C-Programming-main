cf3b3c58230e6c98f618fedc434e129e990ea756
Devansh Meena <devanshmeena422@gmail.com>
2026-05-13 18:48:55 +0530

    add function with parameter

---
+++ 05_Functions/122_function_with_parameter.cpp
@@ -0,0 +1,11 @@
+// Question: Create a function with parameter
+#include <iostream>
+using namespace std;
+void printNumber(int n) {
+    cout << n;
+}
+int main() {
+    int x;
+    cin >> x;
+    printNumber(x);
+}
