580269790a58c8b0b437626133600e5dd82d2d61
Devansh Meena <devanshmeena422@gmail.com>
2026-05-14 22:40:54 +0530

    add digit count function

---
+++ 05_Functions/136_count_digits_using_function.cpp
@@ -0,0 +1,16 @@
+// Question: Count digits using function
+#include <iostream>
+using namespace std;
+int countDigits(int n) {
+    int count = 0;
+    while(n > 0) {
+        count++;
+        n /= 10;
+    }
+    return count;
+}
+int main() {
+    int n;
+    cin >> n;
+    cout << countDigits(n);
+}
