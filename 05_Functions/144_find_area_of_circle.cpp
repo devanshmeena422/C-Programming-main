008c1f5876c3aefb2a93f4cc34d6dc3a0d8ddc99
Devansh Meena <devanshmeena422@gmail.com>
2026-05-14 22:50:40 +0530

    add circle area function

---
+++ 05_Functions/144_find_area_of_circle.cpp
@@ -0,0 +1,11 @@
+// Question: Find area of circle using function
+#include <iostream>
+using namespace std;
+float area(float r) {
+    return 3.14 * r * r;
+}
+int main() {
+    float r;
+    cin >> r;
+    cout << area(r);
+}
