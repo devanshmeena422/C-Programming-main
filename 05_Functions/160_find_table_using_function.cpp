4a9046543cacc97ba797f46aafc7d4cc1303c67d
Devansh Meena <devanshmeena422@gmail.com>
2026-05-15 22:52:33 +0530

    add table function

---
+++ 05_Functions/160_find_table_using_function.cpp
@@ -0,0 +1,12 @@
+// Question: Print multiplication table using function
+#include <iostream>
+using namespace std;
+void table(int n) {
+    for(int i = 1; i <= 10; i++)
+        cout << n << " x " << i << " = " << n * i << endl;
+}
+int main() {
+    int n;
+    cin >> n;
+    table(n);
+}
