8809c9195c6aae8bcbfab27d0142dd9ef4b7d1b3
Devansh Meena <devanshmeena422@gmail.com>
2026-05-19 23:04:10 +0530

    add stack push pop

---
+++ 07_Strings/211_stack_push_pop.cpp
@@ -0,0 +1,12 @@
+// Question: Push and pop elements in stack
+#include <iostream>
+#include <stack>
+using namespace std;
+int main() {
+    stack<int> s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    s.pop();
+    cout << s.top();
+}
