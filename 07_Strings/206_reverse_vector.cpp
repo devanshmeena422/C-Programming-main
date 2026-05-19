cfbba5772dd3a375ed05d99333125cca9df1712b
Devansh Meena <devanshmeena422@gmail.com>
2026-05-19 23:01:48 +0530

    add reverse vector

---
+++ 07_Strings/206_reverse_vector.cpp
@@ -0,0 +1,11 @@
+// Question: Reverse vector elements
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    vector<int> v = {1,2,3,4,5};
+    reverse(v.begin(), v.end());
+    for(int i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+}
