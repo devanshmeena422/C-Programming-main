8f052fe62d57166e2a2a9f3d4b5e25cf1c098811
Devansh Meena <devanshmeena422@gmail.com>
2026-05-19 23:00:23 +0530

    add vector pop back

---
+++ 07_Strings/203_vector_pop_back.cpp
@@ -0,0 +1,10 @@
+// Question: Remove last element using pop_back
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    vector<int> v = {1,2,3,4,5};
+    v.pop_back();
+    for(int i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+}
