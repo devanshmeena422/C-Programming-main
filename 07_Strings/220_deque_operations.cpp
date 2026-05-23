2cb8f5b541f0617a2c72025de0fef2ce957dae5e
Devansh Meena <devanshmeena422@gmail.com>
2026-05-23 18:16:26 +0530

    add deque operations

---
+++ 07_Strings/220_deque_operations.cpp
@@ -0,0 +1,12 @@
+// Question: Perform operations on deque
+#include <iostream>
+#include <deque>
+using namespace std;
+int main() {
+    deque<int> d;
+    d.push_back(10);
+    d.push_front(20);
+    d.push_back(30);
+    for(int i = 0; i < d.size(); i++)
+        cout << d[i] << " ";
+}
