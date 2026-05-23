4d9422a898b8b8151a47b4046b4e3de8bafa74b6
Devansh Meena <devanshmeena422@gmail.com>
2026-05-23 18:13:25 +0530

    new fileadd stack size

---
+++ 07_Strings/212_stack_size.cpp
@@ -0,0 +1,11 @@
+// Question: Find size of stack
+#include <iostream>
+#include <stack>
+using namespace std;
+int main() {
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    cout << s.size();
+}
