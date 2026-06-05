72996e2a5e01a3a9c0e94cc277572d44ab26d048
Devansh Meena <devanshmeena422@gmail.com>
2026-06-06 00:01:34 +0530

    add vector erase

---
+++ 08_stl/226_vector_erase.cpp
@@ -0,0 +1,10 @@
+// Question: Erase element from vector
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    vector<int> v = {1,2,3,4,5};
+    v.erase(v.begin() + 2);
+    for(int x : v)
+        cout << x << " ";
+}
