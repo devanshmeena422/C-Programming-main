a6ddd71afe81cbaa5bd5900dd2ce1ecd9937f882
Devansh Meena <devanshmeena422@gmail.com>
2026-06-06 00:04:02 +0530

    add vector find

---
+++ 08_stl/231_find_element_vector.cpp
@@ -0,0 +1,11 @@
+// Question: Find element using STL find
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    vector<int> v = {10,20,30,40};
+    auto it = find(v.begin(), v.end(), 30);
+    if(it != v.end())
+        cout << "Found";
+}
