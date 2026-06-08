441cc5898c2b408fe0b766b36a6d350fc7ee5ec2
Devansh Meena <devanshmeena422@gmail.com>
2026-06-08 23:41:59 +0530

    add next permutation

---
+++ 08_stl/236_next_permutation.cpp
@@ -0,0 +1,11 @@
+// Question: Generate next permutation
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    vector<int> v = {1,2,3};
+    next_permutation(v.begin(), v.end());
+    for(int x : v)
+        cout << x << " ";
+}
