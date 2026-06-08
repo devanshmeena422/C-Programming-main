cc3ca67d753d486654ecefeea3eb3c88862d62f3
Devansh Meena <devanshmeena422@gmail.com>
2026-06-08 23:40:05 +0530

    add vector count

---
+++ 08_stl/232_count_element_vector.cpp
@@ -0,0 +1,9 @@
+// Question: Count occurrences in vector
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    vector<int> v = {1,2,2,3,2};
+    cout << count(v.begin(), v.end(), 2);
+}
