7daf2246037aa74f3b2ce0993a1413bdaf1c53a2
Devansh Meena <devanshmeena422@gmail.com>
2026-06-09 22:09:59 +0530

    add hierarchical inheritance

---
+++ 09_OOPS/250_hierarchical_inheritance.cpp
@@ -0,0 +1,17 @@
+// Question: Demonstrate hierarchical inheritance
+#include <iostream>
+using namespace std;
+class Parent {
+public:
+    void show() {
+        cout << "Parent";
+    }
+};
+class Child1 : public Parent {
+};
+class Child2 : public Parent {
+};
+int main() {
+    Child1 c1;
+    c1.show();
+}
