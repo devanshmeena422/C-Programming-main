38b9f603a4f4ed05703a33b48aed9f470bcc8108
Devansh Meena <devanshmeena422@gmail.com>
2026-06-12 14:13:35 +0530

    add bubble sort

---
+++ 10_DSA_Basics/262_bubble_sort.cpp
@@ -0,0 +1,17 @@
+// Question: Sort array using Bubble Sort
+#include <iostream>
+using namespace std;
+int main() {
+    int arr[5] = {5,1,4,2,3};
+    for(int i = 0; i < 4; i++) {
+        for(int j = 0; j < 4 - i; j++) {
+            if(arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    for(int i = 0; i < 5; i++)
+        cout << arr[i] << " ";
+}
