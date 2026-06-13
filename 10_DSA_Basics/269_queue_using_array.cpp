d096816d857a8dfc81875216f0bd822525c725b4
Devansh Meena <devanshmeena422@gmail.com>
2026-06-13 23:07:39 +0530

    add queue array

---
+++ 10_DSA_Basics/269_queue_using_array.cpp
@@ -0,0 +1,11 @@
+// Question: Implement Queue using Array
+#include <iostream>
+using namespace std;
+int main() {
+    int queue[5];
+    int front = 0, rear = -1;
+    queue[++rear] = 10;
+    queue[++rear] = 20;
+    queue[++rear] = 30;
+    cout << queue[front];
+}
