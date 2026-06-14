d4342daff75fafaac95500038b18176c2d3d1c34
Devansh Meena <devanshmeena422@gmail.com>
2026-06-14 23:45:08 +0530

    add stack push

---
+++ 10_DSA_Basics/274_push_stack.cpp
@@ -0,0 +1,9 @@
+// Question: Push element into stack
+#include <iostream>
+using namespace std;
+int main() {
+    int stack[5];
+    int top = -1;
+    stack[++top] = 100;
+    cout << stack[top];
+}
