eb6ada71fd93ae1bd2f9e85703dcd395d766b23a
Devansh Meena <devanshmeena422@gmail.com>
2026-06-14 23:45:36 +0530

    add stack pop

---
+++ 10_DSA_Basics/275_pop_stack.cpp
@@ -0,0 +1,9 @@
+// Question: Pop element from stack
+#include <iostream>
+using namespace std;
+int main() {
+    int stack[5] = {10,20,30};
+    int top = 2;
+    top--;
+    cout << stack[top];
+}
