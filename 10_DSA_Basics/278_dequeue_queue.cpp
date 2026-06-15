76e11f6e6d6fa320260e58fb00ae6b98ec026316
Devansh Meena <devanshmeena422@gmail.com>
2026-06-15 22:40:53 +0530

    add dequeue queue

---
+++ 10_DSA_Basics/278_dequeue_queue.cpp
@@ -0,0 +1,9 @@
+// Question: Dequeue element from queue
+#include <iostream>
+using namespace std;
+int main() {
+    int queue[3] = {10,20,30};
+    int front = 0;
+    front++;
+    cout << queue[front];
+}
