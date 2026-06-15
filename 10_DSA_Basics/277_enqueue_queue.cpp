8da5801da35127e6bf835c074190d0720e0c7eaf
Devansh Meena <devanshmeena422@gmail.com>
2026-06-15 22:40:03 +0530

    add enqueue queue

---
+++ 10_DSA_Basics/277_enqueue_queue.cpp
@@ -0,0 +1,9 @@
+// Question: Enqueue element into queue
+#include <iostream>
+using namespace std;
+int main() {
+    int queue[5];
+    int rear = -1;
+    queue[++rear] = 50;
+    cout << queue[rear];
+}
