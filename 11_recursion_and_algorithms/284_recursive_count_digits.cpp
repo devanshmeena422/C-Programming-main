9f03b9522eec48f7bf1d1cb4c99dea55ece36cc9
Devansh Meena <devanshmeena422@gmail.com>
2026-06-17 22:57:14 +0530

    add recursive count digits

---
+++ 11_recursion_and_algorithms/284_recursive_count_digits.cpp
@@ -0,0 +1,13 @@
+// Question: Count digits using recursion
+#include <iostream>
+using namespace std;
+int countDigits(int n) {
+    if(n == 0)
+        return 0;
+    return 1 + countDigits(n / 10);
+}
+int main() {
+    int n;
+    cin >> n;
+    cout << countDigits(n);
+}
