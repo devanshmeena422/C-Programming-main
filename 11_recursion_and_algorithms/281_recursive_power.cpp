a42d7f0ec30164cae2d694a406fe16ecb3f935eb
Devansh Meena <devanshmeena422@gmail.com>
2026-06-17 22:55:49 +0530

    add recursive power

---
+++ 11_recursion_and_algorithms/281_recursive_power.cpp
@@ -0,0 +1,13 @@
+// Question: Find power using recursion
+#include <iostream>
+using namespace std;
+int power(int a, int b) {
+    if(b == 0)
+        return 1;
+    return a * power(a, b - 1);
+}
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << power(a, b);
+}
